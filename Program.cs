b469a1e38c5d8979cf37158a6cd6c236be2302a8
cat32tw <mike1993xd@gmail.com>
2022-02-23 22:55:19 +0800

    字典演練

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Dictionary_Ex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> openWith = new Dictionary<string, string>();
+            openWith.Add("txt", "notepad.exe");
+            openWith.Add("bmp", "paint.exe");
+            openWith.Add("rtf", "wordpad.exe");
+            openWith.Add("dib", "paint.exe");
+            if (!openWith.ContainsKey("doc"))
+            {
+                openWith["doc"] = "winword.exe";
+                Console.WriteLine("新增  Key = \"doc\", Value = {0} \n", openWith["doc"]);
+            }
+
+
+            Console.WriteLine("陳列 Dictionary 泛型物件 Key 與 Value 組配對資料");
+            foreach (KeyValuePair<string, string> kvp in openWith)
+            {
+                Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
+            }
+            Console.Read();
+        }
+    }
+}
